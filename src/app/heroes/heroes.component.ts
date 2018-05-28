ea3831a46fa201ef1c15d69874c70f087c3ea7e0
Tibin <tibinmutholy@gmail.com>
2018-05-28 15:20:18 +0530

    ng if and class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,12 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 heroes = HEROES;
-hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+selectedHero: Hero;
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
 
 
 
